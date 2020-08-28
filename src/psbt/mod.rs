a947564dd8769128fd9c7d49a2040cbd9b2ac0a7
sanket1729 <sanket1729@gmail.com>
2020-08-28 13:42:28 -0500

    Implemnented finalizer

--- src/psbt/mod.rs
+++ src/psbt/mod.rs
@@ -31,6 +31,9 @@ use BitcoinSig;
 use MiniscriptKey;
 use Satisfier;
 
+mod finalizer;
+pub use self::finalizer::{finalize, interpreter_check};
+
 #[derive(Debug)]
 pub enum InputError {
     /// Get the secp Errors directly
@@ -240,7 +243,7 @@ pub fn extract(psbt: &Psbt) -> Result<bitcoin::Transaction, Error> {
         }
     }
     // sanity check that everything works
-    // interpreter_check(&psbt)?;
+    interpreter_check(&psbt)?;
     Ok(ret)
 }
 
