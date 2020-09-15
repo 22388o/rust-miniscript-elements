sgeisler/rust-miniscript

Make Descriptors derivable

src/descriptor/mod.rs
@@ -222,6 +222,34 @@ impl DescriptorKey {
             ))
         }
     }
+
+    /// Derives a new key using the path if self is a wildcard xpub. Otehrwise returns a copy of
+    /// self.
+    ///
+    /// Panics if derivation path contains a hardened child number
+    pub fn derive(&self, path: &[ChildNumber]) -> DescriptorKey {
+        assert!(path.into_iter().all(|c| c.is_normal()));
+
+        match self {
+            DescriptorKey::PukKey(pk) => DescriptorKey::PukKey(*pk),
+            DescriptorKey::XPub(xpub) => {
+                if xpub.is_wildcard {
+                    DescriptorKey::XPub(DescriptorXPub {
+                        source: xpub.source.clone(),
+                        xpub: xpub.xpub.clone(),
+                        derivation_path: (&xpub.derivation_path)
+                            .into_iter()
+                            .chain(path.iter())
+                            .cloned()
+                            .collect(),
+                        is_wildcard: false,
+                    })
+                } else {
+                    self.clone()
+                }
+            }
+        }
+    }
 }
 
 impl MiniscriptKey for DescriptorKey {
@@ -258,6 +286,12 @@ impl ToPublicKey for DescriptorKey {
 
     fn hash_to_hash160(hash: &Self::Hash) -> hash160::Hash {
         *hash
+    }
+}
+
+impl Display for DescriptorKeyParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str(self.0)
     }
 }
 
@@ -570,6 +604,14 @@ impl<Pk: MiniscriptKey + ToPublicKey> Descriptor<Pk> {
                     + ms.max_satisfaction_size(2)
             }
         }
+    }
+}
+
+impl Descriptor<DescriptorKey> {
+    /// Derives all wildcard keys in the descriptor using the supplied `path`
+    pub fn derive(&self, path: &[ChildNumber]) -> Descriptor<DescriptorKey> {
+        self.translate_pk(|pk| Result::<_, ()>::Ok(pk.derive(path)), |pkh| Ok(*pkh))
+            .expect("Translation fn can't fail.")
     }
 }
 
@@ -1142,5 +1184,28 @@ mod tests {
         );
         assert_eq!(expected, key.parse().unwrap());
         assert_eq!(format!("{}", expected), key);
+    }
+
+    #[test]
+    #[cfg(feature = "compiler")]
+    fn parse_and_derive() {
+        let descriptor_str = "thresh(2,\
+pk([d34db33f/44'/0'/0']xpub6ERApfZwUNrhLCkDtcHTcxd75RbzS1ed54G1LkBUHQVHQKqhMkhgbmJbZRkrgZw4koxb5JaHWkY4ALHY2grBGRjaDMzQLcgJvLJuZZvRcEL/1/*),\
+pk(xpub6ERApfZwUNrhLCkDtcHTcxd75RbzS1ed54G1LkBUHQVHQKqhMkhgbmJbZRkrgZw4koxb5JaHWkY4ALHY2grBGRjaDMzQLcgJvLJuZZvRcEL/1),\
+pk(03f28773c2d975288bc7d1d205c3748651b075fbc6610e58cddeeddf8f19405aa8))";
+        let policy: crate::policy::concrete::Policy<DescriptorKey> =
+            descriptor_str.parse().unwrap();
+        let descriptor = Descriptor::Sh(policy.compile().unwrap());
+        let derived_descriptor = descriptor.derive(&[ChildNumber::from_normal_idx(42).unwrap()]);
+
+        let res_descriptor_str = "thresh(2,\
+pk([d34db33f/44'/0'/0']xpub6ERApfZwUNrhLCkDtcHTcxd75RbzS1ed54G1LkBUHQVHQKqhMkhgbmJbZRkrgZw4koxb5JaHWkY4ALHY2grBGRjaDMzQLcgJvLJuZZvRcEL/1/42),\
+pk(xpub6ERApfZwUNrhLCkDtcHTcxd75RbzS1ed54G1LkBUHQVHQKqhMkhgbmJbZRkrgZw4koxb5JaHWkY4ALHY2grBGRjaDMzQLcgJvLJuZZvRcEL/1),\
+pk(03f28773c2d975288bc7d1d205c3748651b075fbc6610e58cddeeddf8f19405aa8))";
+        let res_policy: crate::policy::concrete::Policy<DescriptorKey> =
+            res_descriptor_str.parse().unwrap();
+        let res_descriptor = Descriptor::Sh(res_policy.compile().unwrap());
+
+        assert_eq!(res_descriptor, derived_descriptor);
     }
 }

22388o/rust-miniscript-elements

Signed-off-by: Antoine Poinsot <darosior@protonmail.com>

src/descriptor/mod.rs
@@ -92,6 +92,12 @@ pub struct DescriptorXPub {
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub struct DescriptorKeyParseError(&'static str);
 
+impl fmt::Display for DescriptorKeyParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str(self.0)
+    }
+}
+
 impl fmt::Display for DescriptorPublicKey {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
@@ -223,6 +229,34 @@ impl DescriptorPublicKey {
             Err(DescriptorKeyParseError(
                 "Hardened derivation is currently not supported.",
             ))
+        }
+    }
+
+    /// Derives a new key using the path if self is a wildcard xpub. Otherwise returns self.
+    ///
+    /// Panics if derivation path contains a hardened child number
+    pub fn derive(self, path: &[bip32::ChildNumber]) -> DescriptorPublicKey {
+        assert!(path.into_iter().all(|c| c.is_normal()));
+
+        match self {
+            DescriptorPublicKey::PubKey(_) => self,
+            DescriptorPublicKey::XPub(xpub) => {
+                if xpub.is_wildcard {
+                    DescriptorPublicKey::XPub(DescriptorXPub {
+                        origin: xpub.origin,
+                        xpub: xpub.xpub,
+                        derivation_path: xpub
+                            .derivation_path
+                            .into_iter()
+                            .chain(path.iter())
+                            .cloned()
+                            .collect(),
+                        is_wildcard: false,
+                    })
+                } else {
+                    DescriptorPublicKey::XPub(xpub)
+                }
+            }
         }
     }
 }
@@ -610,6 +644,17 @@ impl<Pk: MiniscriptKey + ToPublicKey> Descriptor<Pk> {
             //       the `scriptCode` is the `witnessScript` serialized as scripts inside CTxOut.
             Descriptor::Wsh(ref d) | Descriptor::ShWsh(ref d) => Ok(d.encode()),
         }
+    }
+}
+
+impl Descriptor<DescriptorPublicKey> {
+    /// Derives all wildcard keys in the descriptor using the supplied `path`
+    pub fn derive(&self, path: &[bip32::ChildNumber]) -> Descriptor<DescriptorPublicKey> {
+        self.translate_pk(
+            |pk| Result::Ok::<DescriptorPublicKey, ()>(pk.clone().derive(path)),
+            |pkh| Result::Ok::<hash160::Hash, ()>(*pkh),
+        )
+        .expect("Translation fn can't fail.")
     }
 }
 